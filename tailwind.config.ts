d09bee3ce66ed207b3b95314184b48f02865ee82
Kaan Kaya <70816118+kaya-kaan@users.noreply.github.com>
2025-02-06 15:18:38 -0500

    Main pages are created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-} satisfies Config;
+};
